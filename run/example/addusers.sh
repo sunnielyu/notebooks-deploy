3b0875cd31e323c9409b10080a6e8bede6bc2352
wadlie <32208408+wadlie@users.noreply.github.com>
2018-06-11 08:57:12 -0400

    Fixed Dockerfile to build
    Changed RUN to CMD in file

--- run/example/addusers.sh
+++ run/example/addusers.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 
-IFS="
-"
+IFS=$'\n'
 for line in `cat userlist`; do
   test -z "$line" && continue
   user=`echo $line | cut -f 1 -d' '`
-  echo "adding user $user"
+  echo "adding user $user"     # the classification part (admin/...)
   useradd -m -s /bin/bash $user
 done
+
+
+# Using a read instead
+#while read userlist; do
+# test -z "$userlist" && continue
+# user=`echo $userlist | cut -f 1 -d' '`
+# echo "adding user $user"
+# useradd -m -s /bin/bash $user
+#done
