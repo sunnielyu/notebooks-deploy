9b8b1d3918d498e834239a1929604045b5453a68
Sunny <sunny22yu@gmail.com>
2019-05-17 11:45:33 -0400

    fix: add dynamic system user creation, admin users

--- run/example/jupyterhub_config.py
+++ run/example/jupyterhub_config.py
@@ -5,29 +5,30 @@ c = get_config()
 c.JupyterHub.log_level = 10
 
 import os
-from oauthenticator.generic import GenericOAuthenticator
+from oauthenticator.generic import LocalGenericOAuthenticator
 
 OAUTH_CLIENT_ID = os.getenv('OAUTH_CLIENT_ID')
 OAUTH_CLIENT_SECRET = os.getenv('OAUTH_CLIENT_SECRET')
-OAUTH_CALLBACK_URL = os.getenv('OAUTH_CALLBACK_URL')
+AUTH_URL = os.getenv('AUTH_URL')
+ADMIN_USERS = os.getenv('ADMIN_USERS')
 
-ORGANIZATION = os.getenv('ORGANIZATION')  # LabShare Auth tenant
-AUTH_URL = os.getenv('AUTH_URL')          # URL of the LabShare Auth instance
+c.JupyterHub.authenticator_class = LocalGenericOAuthenticator
+
+c.Authenticator.admin_users = set(ADMIN_USERS.split(';'))
+
+c.LocalGenericOAuthenticator.client_id = OAUTH_CLIENT_ID
+c.LocalGenericOAuthenticator.client_secret = OAUTH_CLIENT_SECRET
+c.LocalGenericOAuthenticator.userdata_url = "{0}/auth/me".format(AUTH_URL)
+c.LocalGenericOAuthenticator.username_key = "email"
+c.LocalGenericOAuthenticator.userdata_method = "GET"
+c.LocalGenericOAuthenticator.extra_params = dict(client_id=OAUTH_CLIENT_ID, client_secret=OAUTH_CLIENT_SECRET)
+c.LocalGenericOAuthenticator.basic_auth = False
+c.LocalGenericOAuthenticator.create_system_users = True
+c.LocalGenericOAuthenticator.add_user_cmd = ['adduser', '-q', '--gecos', '""', '--disabled-password', '--force-badname']
 
 # Add SSL key and cert
-#c.JupyterHub.ssl_key = '/srv/oauthenticator/ssl/ssl.key'
-#c.JupyterHub.ssl_cert = '/srv/oauthenticator/ssl/ssl.cert'
-
-c.JupyterHub.authenticator_class = GenericOAuthenticator
-
-c.GenericOAuthenticator.oauth_callback_url=OAUTH_CALLBACK_URL
-c.GenericOAuthenticator.client_id = OAUTH_CLIENT_ID
-c.GenericOAuthenticator.client_secret = OAUTH_CLIENT_SECRET
-c.GenericOAuthenticator.userdata_url = "{0}/auth/me".format(AUTH_URL)
-c.GenericOAuthenticator.username_key = "email"
-c.GenericOAuthenticator.username_method = "GET"
-c.GenericOAuthenticator.extra_params = dict(client_id=OAUTH_CLIENT_ID, client_secret=OAUTH_CLIENT_SECRET)
-c.GenericOAuthenticator.basic_auth = False
+c.JupyterHub.ssl_key = '/srv/oauthenticator/ssl/ssl.key'
+c.JupyterHub.ssl_cert = '/srv/oauthenticator/ssl/ssl.cert'
 
 # For local testing
-#c.GenericOAuthenticator.tls_verify = False
+c.LocalGenericOAuthenticator.tls_verify = False
