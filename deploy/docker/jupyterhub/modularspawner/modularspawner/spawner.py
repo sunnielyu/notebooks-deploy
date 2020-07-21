sunnielyu/notebooks-deploy

commit b40789e0d4b8a9451d329714001c2c2968fda249

fix: Form string correctly (#92)
Fixes the incorrectly formed string for the case when there are no additional stacks chosen. In the current implementation the string 'railyard hash -b <self.base> ' is formed with the trailing space. When applying `split` it returns list ['railyard', 'hash', '-b', '<self.base>', '']. The last element of the list is passed to subprocess as the empty argument, and `railyard` CLI fails with `Error: Unexpected argument:`. As a result `tag` variable is not set in line 42 and spawn fails.

deploy/docker/jupyterhub/modularspawner/modularspawner/spawner.py
@@ -46,7 +46,7 @@ class ModularSpawner(KubeSpawner):
             #if image does not exist, switch to the maximal image with all stacks included
             self.log.debug("Requested tag %s is not in registry, using default image", tag)
             options = [True] * len(self.stacks)
-            tag = subprocess.run(('railyard hash ' + '-b ' + self.base + ' ' + ' '.join([f'-a {item}' for stack,included in zip(self.stacks,options) for item in stack if included])).split(' '), capture_output=True).stdout.decode("utf-8").rstrip()
+            tag = subprocess.run(('railyard hash ' + '-b ' + self.base + ''.join([f' -a {item}' for stack,included in zip(self.stacks,options) for item in stack if included])).split(' '), capture_output=True).stdout.decode("utf-8").rstrip()
         
         # Get full image tag
         image = 'labshare/polyglot-notebook:' + tag

commit f53bd33a65f665ea12e45109c1c375ccee6dda36

Fix issues introduced by #92 (#97)
* fix: Bumped versions
* JupyterHub image
* modularspawner package
* fix: Fix wrong nodejs version and wrapper command
* fix: bump modularspawner version

deploy/docker/jupyterhub/Dockerfile
@@ -27,7 +27,7 @@ RUN wget -q https://repo.continuum.io/miniconda/Miniconda3-py38_4.8.2-Linux-x86_
     bash /tmp/miniconda.sh -f -b -p /opt/conda && \
     /opt/conda/bin/conda install --yes -c conda-forge \
       sqlalchemy tornado jinja2 traitlets requests pip pycurl \
-      nodejs configurable-http-proxy && \
+      nodejs=12 configurable-http-proxy && \
     /opt/conda/bin/pip install --upgrade pip && \
     /opt/conda/bin/pip install jupyterhub==1.0.0 \
       git+https://github.com/labshare/oauthenticator.git \

deploy/docker/jupyterhub/VERSION
@@ -1,1 +1,1 @@
-0.4.1
+0.4.2

deploy/docker/jupyterhub/modularspawner/modularspawner/spawner.py
@@ -20,7 +20,7 @@ class ModularSpawner(KubeSpawner):
         
         self.form = """
 <div class="form-group">
-    Choose language kernels and/or packages:
+    Choose additional language kernels and/or packages. Loads with Python kernel by default:
     <br>"""
         
         for i in range(len(self.stacks)):
@@ -38,8 +38,8 @@ class ModularSpawner(KubeSpawner):
         # Decode user choices        
         options = [True if formdata.get(option, None) else False for option in self.options]
         
-        # Get image hash by running `polus-railyard` 
-        tag = subprocess.run(('railyard hash ' + '-b ' + self.base + ' ' + ' '.join([f'-a {item}' for stack,included in zip(self.stacks,options) for item in stack if included])).split(' '), capture_output=True).stdout.decode("utf-8").rstrip()
+        # Get image hash by running `polus-railyard`
+        tag = subprocess.run(('railyard hash ' + '-b ' + self.base + ''.join([f' -a {item}' for stack,included in zip(self.stacks,options) for item in stack if included])).split(' '), capture_output=True).stdout.decode("utf-8").rstrip()
 
         #Check if image exist to avoid PullErrors
         if requests.get('https://hub.docker.com/v2/repositories/labshare/polyglot-notebook/tags/' + tag).status_code != 200:

deploy/docker/jupyterhub/modularspawner/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='Modular Spawner',
-      version='0.1.1',
+      version='0.1.2',
       description='Modular JupyterHub spawner based on Kubespawner',
       author='Konstantin Taletskiy',
       author_email='konstantin.taletskiy@labshare.org',